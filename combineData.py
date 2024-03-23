5e104f13b6ca385c1f631c0a176eef27cb84eaec
EDaley-FlemCollege <eric.daley@flemingcollege.ca>
2024-03-22 20:39:43 -0400

    Updated CombineData.py

--- combineData.py
+++ combineData.py
@@ -7,6 +7,8 @@ df2 = pd.read_csv('perCapita.csv')
 # Merge the dataframes based on the "YEAR" and "COUNTRY" columns
 mergedDf = pd.merge(df1, df2, on=['Year', 'Country'])
 
+mergedDf = mergedDf[['Year', 'Country', 'Mean_deforestation', 'Mean_forest-regrowth', 'Mean_wood-harvest', 'Mean_GFED4_peat', 'Per-capita_Per-capita territorial']]
+
 # Output the merged dataframe to a CSV file
 mergedDf.to_csv('mergedData.csv', index=False)
 
